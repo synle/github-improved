0a30392493a7361dade45b27affbf078e6af1eef
Sy Le <lenguyensy+mbp@gmail.com>
2016-12-22 08:34:07 -0800

    clean up pr nav panels

--- src/inject/js/component/prNavigation.js
+++ src/inject/js/component/prNavigation.js
@@ -4,26 +4,26 @@ import _ from 'lodash';
 
 //internal
 import sidebarUtil from '@src/util/sidebarUtil';
+import Panel from '@src/component/panel';
 
 const PRNavigation = React.createClass({
   render() {
       const { visible } = this.props;
 
       if(visible){
-        return (
-          <div className="panel panel-primary">
-              <div className="panel-heading">
-                  <h4>Pull Request</h4>
-              </div>
-              <div className="panel-body padding-top0">
-                <div id="side-bar-pr-toolbox"></div>
-                <div>
-                    <button onClick={sidebarUtil.onGoToOwnPRUrl} className="btn btn-sm">PR created by you</button>
-                    <button onClick={sidebarUtil.onGoToAssignedPRUrl} className="btn btn-sm">PR assigned to you</button>
-                    <button onClick={sidebarUtil.onGoToOwnPRUrl} className="btn btn-sm">PR mentioning you</button>
-                </div>
-              </div>
+        const domHeader = 'Pull Request';
+        const domBody = (
+          <div>
+            <button onClick={sidebarUtil.onGoToOwnPRUrl} className="btn btn-sm">PR created by you</button>
+            <button onClick={sidebarUtil.onGoToAssignedPRUrl} className="btn btn-sm">PR assigned to you</button>
+            <button onClick={sidebarUtil.onGoToOwnPRUrl} className="btn btn-sm">PR mentioning you</button>
           </div>
+        );
+        return (
+          <Panel domHeader={domHeader}
+            domBody={domBody}
+            isExpanded={true}
+            />
         );
       } else {
         return null;
