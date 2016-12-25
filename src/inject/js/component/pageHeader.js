92477614e4b1d8441a9f1366cf4ef29501e9c763
Sy Le <lenguyensy+mbp@gmail.com>
2016-12-24 17:32:03 -0800

    show or hide sidebar

--- src/inject/js/component/pageHeader.js
+++ src/inject/js/component/pageHeader.js
@@ -4,7 +4,10 @@ import { connect } from 'react-redux';
 const PageHeader = React.createClass({
   render: function() {
     return (
-      <h3 id="side-bar-title ta-center">Github Improved</h3>
+      <h3 id="side-bar-title" className="flex-row">
+        <span className="flex-grow1">Github Improved</span>
+        <span className="flex-shrink0">+/-</span>
+      </h3>
     );
   }
 });
