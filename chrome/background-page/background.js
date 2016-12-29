7614bdda5d7b1a56069edbcd53da8c8e0dc9a270
Sy Le <lenguyensy+mbp@gmail.com>
2016-12-29 14:57:26 -0800

    build

--- chrome/background-page/background.js
+++ chrome/background-page/background.js
@@ -1,19 +1,6 @@
 console.log('background page init...');
 
 // function makeRequest(url, data){
-//   //                              /repos/:owner/:repo/pulls/:number
-//   var owner= 'relateiq';
-//   var repo = 'riq';
-//   var pullRequestNumber = '6946';
-//   var auth_token = '...';
-
-//   var url;
-//   url = `https://api.github.com/repos/${owner}/${repo}/pulls/${pullRequestNumber}/commits`;
-//   // url = `https://api.github.com/users/octocat/orgs`;
-//   // url = `https://api.github.com`;
-//   // url = `https://api.github.com/user?access_token=${auth_token}`
-//   // url = `https://api.github.com/users/synle`;
-
 //   var request = new Request(url, {
 //     method: 'GET',
 //     mode: 'cors',
