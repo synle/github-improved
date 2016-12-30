099469e9bb1a0d9b6db1e29c01d8fd0a0940d68c
Sy Le <lenguyensy+mbp@gmail.com>
2016-12-30 10:29:58 -0800

    showing pending pull requests in current repo...

--- src/inject/js/component/prNavigation.js
+++ src/inject/js/component/prNavigation.js
@@ -5,6 +5,9 @@ import _ from 'lodash';
 //internal
 import sidebarUtil from '@src/util/sidebarUtil';
 import Panel from '@src/component/panel';
+import Pagination from '@src/component/pagination';
+
+const PAGE_SIZE_PULL_REQUEST = 5;
 
 const PRNavigation = React.createClass({
   render() {
@@ -12,11 +15,24 @@ const PRNavigation = React.createClass({
 
       if(visible){
         const domHeader = 'Pull Request';
+        const domPullRequestList = pullRequests.map(
+          pr => {
+            const domKey = `pr-${pr.id}`;
+
+            return (
+              <div key={domKey} className="small-text border-bottom">
+                <a href={pr.url}>{pr.title}</a>
+              </div>
+            );
+          }
+        );
         const domBody = (
           <div>
             <button onClick={sidebarUtil.onGoToOwnPRUrl} className="btn btn-sm">PR created by you</button>
             <button onClick={sidebarUtil.onGoToAssignedPRUrl} className="btn btn-sm margin-top0">PR assigned to you</button>
             <button onClick={sidebarUtil.onGoToOwnPRUrl} className="btn btn-sm margin-top0">PR mentioning you</button>
+
+            <Pagination domList={domPullRequestList} pageSize={PAGE_SIZE_PULL_REQUEST}></Pagination>
           </div>
         );
         return (
@@ -35,7 +51,7 @@ const PRNavigation = React.createClass({
 const mapStateToProps = function(state) {
   return {
     visible: _.get( state, 'ui.visible.prNavBox'),
-    pullRequests: _.get( state, 'repo.pullRequests'),
+    pullRequests: _.get( state, 'repo.pullRequests') || [],
   };
 }
 
