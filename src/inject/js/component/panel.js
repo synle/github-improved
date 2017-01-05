185dc043bd912adae3a42dbbf89013bcc569e000
Sy Le <lenguyensy+mbp@gmail.com>
2017-01-04 20:10:28 -0800

    expand state for panels...

--- src/inject/js/component/panel.js
+++ src/inject/js/component/panel.js
@@ -13,22 +13,26 @@ const Panel = React.createClass({
   },
   render() {
     let {domHeader, domBody} = this.props;
+    let domExpandStateIcon;
     const {isExpanded} = this.state;
 
     if(!isExpanded){
       domBody = null;
+      domExpandStateIcon = <span>&#9660;</span>; // ^
     } else {
       domBody = (
         <div className="panel-body padding0">
           {domBody}
         </div>
       );
+      domExpandStateIcon = <span>&#9650;</span>; // v
     }
 
     return(
       <div className="panel panel-primary">
-        <div className="panel-heading" onClick={e => this.onToggleIsExpanded()}>
-          <h4>{domHeader}</h4>
+        <div className="panel-heading flex-row cpointer" onClick={e => this.onToggleIsExpanded()}>
+          <h4 className="flex-grow1">{domHeader}</h4>
+          <span className="flex-shrink0">{domExpandStateIcon}</span>
         </div>
         {domBody}
       </div>
