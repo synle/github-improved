3dc103a6f948bc1b25d30d49c16465b50a1a8863
Sy Le <sle@salesforce.com>
2017-01-31 10:56:30 -0800

    get something to show up in the new tab page...

--- src/newtab/index.js
+++ src/newtab/index.js
@@ -1,1 +1,30 @@
 console.log('aaaa')
+
+import _ from 'lodash';
+
+import dataUtil from '@src/util/dataUtil';
+import restUtil from '@src/util/restUtil';
+
+dataUtil.getPersistedProp('api-token')
+  .then(
+    (apiToken) =>{
+      if(_.size(apiToken) > 0){
+        // new rest api
+        restUtil.setAuthToken(apiToken);
+
+        //do api calls
+        dataUtil.fetchFeeds().then( function(data){
+          var containerFeeds = $('<pre />').appendTo('#container-feeds')
+            .html(JSON.stringify(data, null, 2))
+        }, console.error);
+        dataUtil.fetchNotifications().then( function(data){
+          var containerNotifications = $('<pre />').appendTo('#container-notifications')
+            .html(JSON.stringify(data, null, 2))
+        }, console.error);
+        dataUtil.fetchEvents().then( function(data){
+          var containerEvents = $('<pre />').appendTo('#container-events')
+            .html(JSON.stringify(data, null, 2))
+        }, console.error);
+      }
+    }
+  )
